e647e41c250c68e382f133049c6c140a76435aa3
Krushna Vetal <161699819+VetalKrushna@users.noreply.github.com>
2025-07-01 22:43:04 +0530

    Add files via upload

---
+++ AWT/a1.java
@@ -0,0 +1,15 @@
+package awt;
+import java.awt.*;
+public class a1  
+{
+	public static void main(String[] args) 
+	{
+	Frame f=new Frame();
+	f.setVisible(true);
+	f.setSize(500,500);
+	f.setBackground(Color.pink);
+	f.setTitle("Frame");
+	f.setLocation(500,200);
+		
+	}
+}
